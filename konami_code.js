92638be34726b240882557038568c3a9a2149dbc
Carl Steele <casteeleu@gmail.com>
2018-08-01 14:56:03 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+const body = document.getElementById("konami-body")
+body.addEventListener('keydown', init) 
+
+
+
+function init(e) { onKeyDownHandler(e) const key = 
+
   // Write your JavaScript code inside the init() function
 
-}
+
